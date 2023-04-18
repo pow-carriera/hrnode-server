986b1598e1aae3e70228aca5c09f8554fe329d3e
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-04-18 08:59:45 +0800

    upload endpoint for img files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { PORT, APPNAME, HOST } from "./utils/config";
+import { PORT } from "./utils/config";
 import * as routes from "./routes/routes";
 import morgan from "morgan";
 import path from "path";
 import cors from "cors";
 import { startMessage } from "./utils/init";
+import fileUpload from "express-fileupload";
 
 const app = express();
 const apiRouter = express.Router();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(morgan("common"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
 
 app.get("/", morgan("common"), (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
@@ -21,6 +23,22 @@ app.use("/api", apiRouter);
 
 apiRouter.use("/users", routes.userRouter);
 apiRouter.use("/auth", routes.authRouter);
+
+app.post("/upload", (req, res) => {
+  try {
+    const img = req.files!.img;
+    console.log(img);
+    res.status(200).json({
+      status: 200,
+      message: "Successful upload."
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: "Bad Request."
+    });
+  }
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({
