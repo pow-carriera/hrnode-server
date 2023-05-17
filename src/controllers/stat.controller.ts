a51a485064134bf9920f23d246326bcb341f9ed6
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-05-17 22:12:20 +0800

    [Change] Remove late, add remove for /stat/users

--- src/controllers/stat.controller.ts
+++ src/controllers/stat.controller.ts
@@ -2,14 +2,56 @@ import { userInfo } from "os";
 import { db } from "../utils/database";
 import moment from "moment";
 
-export const readTimeRecordStats = async () => {
+export const readTimeRecordStats = async (date = moment().format("L")) => {
   return db.timeRecord.count({
     where: {
-      recordDate: moment().format("L")
+      recordDate: date
     },
     select: {
       timeIn: true,
       timeOut: true
     }
   });
+};
+
+export const readPresentEmployees = async (date = moment().format("L")) => {
+  return db.user.count({
+    where: {
+      OR: [
+        {
+          timeRecord: {
+            some: {
+              recordDate: date,
+              remark: "Early"
+            }
+          }
+        },
+        {
+          timeRecord: {
+            some: {
+              recordDate: date,
+              remark: "OnTime"
+            }
+          }
+        }
+      ]
+    }
+  });
+};
+
+export const readOnLeaveEmployees = async (date = moment().format("L")) => {
+  return db.user.count({
+    where: {
+      timeRecord: {
+        some: {
+          recordDate: date,
+          remark: "Leave"
+        }
+      }
+    }
+  });
+};
+
+export const readTotalEmployees = async () => {
+  return db.user.count();
 };
--- src/routes/stat.router.ts
+++ src/routes/stat.router.ts
@@ -3,6 +3,18 @@ import * as statService from "../controllers/stat.controller";
 export const statRouter = express.Router();
 
 statRouter.get("/attendances", async (req, res, next) => {
-  const data = await statService.readTimeRecordStats();
+  const data = await statService.readTimeRecordStats(req.query.date as string);
+  res.status(200).json({ data });
+});
+
+statRouter.get("/users", async (req, res, next) => {
+  const data = {
+    present: await statService.readPresentEmployees(),
+    leave: await statService.readOnLeaveEmployees(),
+    total: await statService.readTotalEmployees(),
+    absent: 0
+  };
+  data.absent = data.total - data.present;
+
   res.status(200).json({ data });
 });
